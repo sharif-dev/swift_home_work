3035e3acd644e28ab6ed61f08561c97e26a238d2
MMDBadCoder <59317678+MMDBadCoder@users.noreply.github.com>
2020-06-26 21:20:07 +0430

    Update finder.swift
    cmplt new functions

--- finder.swift
+++ finder.swift
@@ -16,6 +16,33 @@ struct Coordinate: Hashable {
   let col: Int
 }
 
+extension String {
+
+    var length: Int {
+        return count
+    }
+
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
+
 class TrieNode {
   var letter: Character
   var children: 
@@ -84,4 +111,64 @@ class Trie {
   }
 
 }
+
+
+
+func getNeighbors(m: Int, n: Int, point: Coordinate) -> Set<Coordinate>{
+  var result = Set<Coordinate>()
+  for delta in deltas{
+    if(point.col + delta.dc >= 0 && point.col + delta.dc < n
+      && point.row + delta.dr >= 0 && point.row + delta.dr < m){
+      result.insert(Coordinate(row: point.row + delta.dr, col: point.col + delta.dc))
+    }
+  }
+  return result
+}
+
+func findString(m: Int, n: Int, letterDictionary: inout Dictionary<Character, Set<Coordinate>>, from: Coordinate?, visiteds: Set<Coordinate>, str: String) -> Bool{
+  if(str.count == 0){
+    return true
+  }
+  let firstChar = Character(str[0])
+  if(letterDictionary[firstChar] == nil){
+    return false
+  }
+  var points = letterDictionary[firstChar]!
+  if(from != nil){
+    points = points.intersection(getNeighbors(m: m, n: n, point: from!))
+  }
+  points = points.subtracting(visiteds)
+  if(points.count == 0){
+    return false
+  }
+  var res = false
+  for point in points{
+    var newVisiteds = visiteds
+    newVisiteds.insert(point)
+    res = res || findString(m: m, n: n, letterDictionary: &letterDictionary, from: point, visiteds: newVisiteds, str: str[1..<str.count])
+  }
+  return res
+}
+
+
+var words = readLine()!.components(separatedBy: " ")
+var trie = Trie(words: words)
+var letterLocations = Dictionary<Character, Set<Coordinate>>()
+var mn = readLine()!.components(separatedBy: " ")
+var m = Int(mn[0])!
+var n = Int(mn[1])!
+var table = Array(repeating: Array(repeating: Character("#"), count: n), count: m)
+for i in 0...m-1{
+  var row = readLine()!.components(separatedBy: " ")
+  for j in 0...n-1{
+    table[i][j] = Character(row[j])
+    if(letterLocations[Character(row[j])] == nil){
+      letterLocations[Character(row[j])] = Set<Coordinate>()
+    }
+    letterLocations[Character(row[j])]!.insert(Coordinate(row: i, col: j))
+  }
+}
+trie.getWords(m: m, n: n, letterDictionary: &letterLocations)
+
+
 
